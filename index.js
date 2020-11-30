a8da1ca26e3cc41cc8ffbd90e872a261b12bef17
eri <eri0.sagittarius@gmail.com>
2020-11-30 09:46:58 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let greeting = 'Hello World!';
+// split the string into individual characters
+let splitString = greeting.split('');
+// reverse the character array
+let reverseArray = splitString.reverse();
+// rejoin the array into a string
+let jointArray = reverseArray.join('');
+console.log(jointArray);
